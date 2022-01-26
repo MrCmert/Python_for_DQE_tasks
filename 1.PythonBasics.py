405c0b184752ee4b8851fbb15e1bd0d194fb6920
Vitalii Stetsenko <mrcmert19980701@gmail.com>
2022-01-26 13:44:23 +0200

    Adding sorting

--- 1.PythonBasics.py
+++ 1.PythonBasics.py
@@ -1,12 +1,3 @@
-# Python_for_DQE_tasks
-#Create a python script:
-
-#create list of 100 random numbers from 0 to 1000
-#calculate average for even and odd numbers
-#print both average result in console
-#Each line of code should be commented with description.
-
-#Commit script to git repository and provide link as home task result.
 import random #module for generate random int
 
 list = [] #create empthy list
@@ -15,22 +6,22 @@ list = [] #create empthy list
 for i in range(0, 100):
     list.append(random.randint(0, 1000))
 
+print(list)
 #sort list from min to max (without using sort())
-'''sorted_list = []
-for i in list:
-    if len(sorted_list) == 0:
-        sorted_list.append(i)
-    else:
-        for j in range(0, len(sorted_list)):
-            if i < sorted_list[j]:
-                sorted_list.insert(j, i)'''
+sorted_list = []
+for i in range(0, len(list)):
+    sorted_list.append(min(list))  #find min of list and append to new list
+    list.pop(list.index(min(list)))  #delete min from old list
+
+print(sorted_list)
 
 sum_odd = 0
 count_odd = 0
 sum_even = 0
 count_even = 0
 
-for i in list:
+#calculate sum and count of evens and odds
+for i in sorted_list:
     if i % 2 == 0:
         sum_even = sum_even + i
         count_even = count_even + 1
@@ -38,9 +29,13 @@ for i in list:
         sum_odd = sum_odd + i
         count_odd = count_even + 1
 
-print("Avg even: " + str(sum_even/count_even))
-print("Avg odd: " + str(sum_odd/count_odd))
+#exceptions if there are no even or odd numbers in the list
+try:
+    print("Avg even numbers: " + str(sum_even/count_even))
+except ZeroDivisionError:
+    print("Count even is " + str(count_even))
 
-
-print(list)
-#print(sorted_list)
+try:
+    print("Avg odd numbers: " + str(sum_odd / count_odd))
+except ZeroDivisionError:
+    print("Count odd is " + str(count_odd))
