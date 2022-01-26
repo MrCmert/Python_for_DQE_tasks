321133a40cf2505099b1267849c43c637b34fe7d
Vitalii Stetsenko <mrcmert19980701@gmail.com>
2022-01-26 15:41:21 +0200

    generate list of dicts

---
+++ 2_Collections.py
@@ -0,0 +1,31 @@
+#1. create a list of random number of dicts (from 2 to 10)
+
+#dict's random numbers of keys should be letter,
+#dict's values should be a number (0-100),
+#example: [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+#2. get previously generated list of dicts and create one common dict:
+
+#if dicts have same key, we will take max value, and rename key with dict number with max value
+#if key is only in one dict - take it as is,
+#example: {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+#Each line of code should be commented with description.
+
+# module for generate random int
+import random
+
+# generate number of dicts
+number_of_dicts = random.randint(2, 10)
+# letters for keys
+letters = 'abcdefghijklmnopqrstuvwxyz'
+# empty list
+list_of_dicts = []
+
+# fill list with dicts with random size and random key/value pair
+for i in range(number_of_dicts):
+    new_dict = {}
+    elements_in_dict = random.randint(2, 10)
+    for j in range(elements_in_dict):
+        new_dict[letters[random.randint(0, len(letters)-1)]] = random.randint(0,100)
+    list_of_dicts.append(new_dict)
+
+print(list_of_dicts)
