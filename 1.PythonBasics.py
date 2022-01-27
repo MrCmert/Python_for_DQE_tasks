6b1377614a257bc718a7c46b1c3acd0b7461d47b
Vitalii Stetsenko <mrcmert19980701@gmail.com>
2022-01-27 14:59:53 +0200

    minor changes for first task

--- 1.PythonBasics.py
+++ 1.PythonBasics.py
@@ -1,5 +1,5 @@
-import random # module for generate random int
-
+# module for generate random int
+import random
 
 # generate 100 random numbers and put it to list using list comprehensions
 list = [random.randint(0, 1000) for i in range(0, 100)]
@@ -11,34 +11,36 @@ print(list)
 for i in range(len(list)):
     # iteration by all elements except last and already sorted
     for j in range(0, len(list) - i - 1):
-        # compare element with others
+        # compare element with next
         if list[j] > list[j + 1]:
-            # if this element more than another replace them. set max element in the end
+            # if this element more than next replace them. set max element in the end
             list[j], list[j+1] = list[j+1], list[j]
 
 print(list)
 
-sum_odd = 0
-count_odd = 0
-sum_even = 0
-count_even = 0
+# create lists for odd and even
+list_odd = []
+list_even = []
 
-# calculate sum and count of evens and odds
+# filter odds and evens to different lists
 for i in list:
     if i % 2 == 0:
-        sum_even = sum_even + i
-        count_even = count_even + 1
-    else:
-        sum_odd = sum_odd + i
-        count_odd = count_even + 1
+        list_even.append(i)
+    elif i % 2 != 0:
+        list_odd.append(i)
 
-# exceptions if there are no even or odd numbers in the list
+# calculate avg even and handling division by 0
 try:
-    print("Avg even numbers: " + str(sum_even/count_even))
+    avg_even = sum(list_even)/len(list_even)
 except ZeroDivisionError:
-    print("Count even is " + str(count_even))
+    avg_even = 0
 
+# same for odds
 try:
-    print("Avg odd numbers: " + str(sum_odd / count_odd))
+    avg_odd = sum(list_odd) / len(list_odd)
 except ZeroDivisionError:
-    print("Count odd is " + str(count_odd))
+    avg_odd = 0
+
+# print avg
+print("Avg even numbers: " + str(avg_even))
+print("Avg odd numbers: " + str(avg_odd))
